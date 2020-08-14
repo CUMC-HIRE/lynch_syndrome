0d38bbae586d2c7b9ec690ba04d0e48a384c10b5
Brianna (laptop) <brianna.lauren130@gmail.com>
2020-08-14 15:47:46 -0700

    removed calibration.py for merging

--- Source/calibration.py
+++
@@ -1,7 +0,0 @@
-# Calibrates transition probabilities based on adenoma and CRC incidence data
-import lynch_presets as ps
-import lynch_simulator as sim
-
-# Method: simulated annealing? random search?
-
-# Outline
